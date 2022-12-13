bf0e6ec7f6ef4c577a181fd1423bb43cd6b48e79
Signoregregio <luca030411@gmail.com>
2022-12-13 16:32:33 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MailLogoComponent } from './mail-logo/mail-logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MailLogoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mail-logo/mail-logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mail-logo',
+  templateUrl: './mail-logo.component.html',
+  styleUrls: ['./mail-logo.component.css']
+})
+export class MailLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
