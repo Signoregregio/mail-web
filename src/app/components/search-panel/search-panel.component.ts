5535237c7137456c9ef6d4116753f7f0c61e32c1
Signoregregio <luca030411@gmail.com>
2022-12-20 15:33:58 +0100

    debounce time added

--- src/app/components/search-panel/search-panel.component.ts
+++ src/app/components/search-panel/search-panel.component.ts
@@ -1,16 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { debounceTime, map, of, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-search-panel',
   templateUrl: './search-panel.component.html',
 })
-export class SearchPanelComponent {
+export class SearchPanelComponent implements OnInit {
   @Output()
-  queryInserted = new EventEmitter<any>()
+  queryInserted = new EventEmitter<any>();
 
   query = '';
+  private subjectKeyUp = new Subject<any>();
 
-  onChange(query: string){
-    this.queryInserted.emit(query)
+  ngOnInit(): void {
+    this.subjectKeyUp
+      .pipe(
+        map((event) => event.target.value),
+        debounceTime(500)
+      )
+      .subscribe((debouncedQuery) => this.queryInserted.emit(debouncedQuery));
+  }
+  onChange(query: any) {
+    this.subjectKeyUp.next(query);
   }
 }
